7487905ce321f72a05a405e885df7f0d2d47a766
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-12-13 19:14:35 +0530

    Commit changes...

---
+++ mykubernetesexamples/springexamples/springkuberhelmdemo/src/test/java/com/springkuber/springkuberdemo/SpringkuberdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springkuber.springkuberdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringkuberdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
