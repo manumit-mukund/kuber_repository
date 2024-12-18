e2d2dbe39d8d700d119192a0adaf1e9c8cf839fa
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-12-18 21:24:46 +0530

    First commit...

---
+++ kubernetesexamples/springexamples/springforkuberingress-rewrite-target/src/main/java/com/example/springforkuberingress/SpringBootAppApplication.java
@@ -0,0 +1,22 @@
+package com.example.springforkuberingress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/kuber")
+public class SpringBootAppApplication {
+
+	@RequestMapping("/hello")
+	public String home() {
+		return "Hello Kubernetes for Ingress: for v2!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppApplication.class, args);
+	}
+
+}
