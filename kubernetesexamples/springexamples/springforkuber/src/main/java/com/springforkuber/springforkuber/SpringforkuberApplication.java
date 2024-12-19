ea184bda794c8010e2e74fdd1b95b1daedda0884
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-12-19 10:58:23 +0530

    Commit changes...

--- kubernetesexamples/springexamples/springforkuber/src/main/java/com/springforkuber/springforkuber/SpringforkuberApplication.java
+++ kubernetesexamples/springexamples/springforkuber/src/main/java/com/springforkuber/springforkuber/SpringforkuberApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Date;
 
 @SpringBootApplication
 @RestController
@@ -13,7 +14,7 @@ public class SpringforkuberApplication {
 
 	@GetMapping("/hello")
 	public String sayHello() {
-		return "Hello World for Kuber";
+		return "Hello World for Kuber on " +   new Date();
 	}
 
 	public static void main(String[] args) {
