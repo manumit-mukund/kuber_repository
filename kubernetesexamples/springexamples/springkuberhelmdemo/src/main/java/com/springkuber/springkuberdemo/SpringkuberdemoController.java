f2f791343eea045852bacc96dd5f3a20dc7d039e
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-12-27 19:23:30 +0530

    Commit changes...

--- kubernetesexamples/springexamples/springkuberhelmdemo/src/main/java/com/springkuber/springkuberdemo/SpringkuberdemoController.java
+++ kubernetesexamples/springexamples/springkuberhelmdemo/src/main/java/com/springkuber/springkuberdemo/SpringkuberdemoController.java
@@ -3,6 +3,7 @@ package com.springkuber.springkuberdemo;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/kuber")
@@ -11,7 +12,7 @@ public class SpringkuberdemoController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String syHello() {
-        return "Hello World from Kubernetes 3.0!";
+        return "Hello World from Kubernetes helm for spring-app-chart-1-0 on " +  new Date();
     }
 
 }
