8d11b935a20eef214251b545e2d59acd64b8dbce
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-07-09 20:03:15 +0530

    Commit changes...

--- kubernetesexamples/springexamples/springforkuberhpa/src/main/java/com/example/kuberhpa/KuberhpaController.java
+++ kubernetesexamples/springexamples/springforkuberhpa/src/main/java/com/example/kuberhpa/KuberhpaController.java
@@ -6,43 +6,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.io.IOException;
-import java.util.logging.*;
+
 
 @RestController
 @RequestMapping("/kuber")
 @CrossOrigin
 
-public class UtilityController {
-
-  private static final Logger logger = Logger.getLogger(UtilityController.class.getName());
+public class KuberhpaController {
 
   @RequestMapping(value = "/hpatest", method = RequestMethod.GET)
   public String generateStress() throws IOException, InterruptedException {
 
-    logger.info("HPA test...");
-
-    new CPUSpikerThread().start();
-    new CPUSpikerThread().start();
-    new CPUSpikerThread().start();
-    new CPUSpikerThread().start();
-    new CPUSpikerThread().start();
-    new CPUSpikerThread().start();
+    new MyThread().start();
+    new MyThread().start();
+    new MyThread().start();
+    new MyThread().start();
+    new MyThread().start();
+    new MyThread().start();
 
     return " Hello!->6 threads launched!";
 
-    //Test url: http://localhost:9001/kuber/hpatest
+    // Test url: http://localhost:9001/kuber/hpatest
   }
 
 }
 
-class CPUSpikerThread extends Thread {
+class MyThread extends Thread {
 
   @Override
   public void run() {
 
     while (true) {
 
-      // Just looping infinitely
     }
   }
 }
